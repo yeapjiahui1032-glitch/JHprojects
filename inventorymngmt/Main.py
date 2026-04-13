1ebcf6b863e9c385df8bf38f57150523b309b057
yeapjiahui1032-glitch <yeapjiahui1032@gmail.com>
2026-04-13 14:34:19 +0800

    Refactor reduce_quantity_form: Improve result handling for quantity reduction feedback

--- inventorymngmt/Main.py
+++ inventorymngmt/Main.py
@@ -58,10 +58,10 @@ with col1:
         reduce_submitted = st.form_submit_button("Reduce Quantity")
         if reduce_submitted:
             result = reduce_product_quantity(reduce_id, reduce_quantity)
-    if result is True:
-        st.success("Quantity reduced successfully!")
-    else:
-        st.error("Failed! Check the SKU or quantity entered.")
+            if result is True:
+                st.success("Quantity reduced successfully!")
+        else:
+            st.error("Failed! Check the SKU or quantity entered.")
 with col2:
     with st.form("increase_quantity_form"):
         increase_id = st.text_input("Product SKU to Increase")
